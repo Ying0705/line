5bd67bc744b51de2f816d57007d02c4460bd76bc
Ying0705 <yy147716@gmail.com>
2022-06-07 20:23:51 +0800

    fix3

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = 'What do you ask?'
+    r = 'Im sorry what do you ask'
 
     if msg == 'hi' :
-        r == 'hi'  
+        r == 'hi'
     elif msg == 'Do you eat at all?' :
-        r == 'not yet'
+        r == 'Not yet'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r)
