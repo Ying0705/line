8d1c99fd79f434466271e0e4ee96b9bb162e069f
Ying0705 <yy147716@gmail.com>
2022-06-07 19:59:50 +0800

    new app

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('貼上你的line bot channel token')
-handler = WebhookHandler('貼上你的line bot channel secret')
+line_bot_api = LineBotApi('KIwbm4b9dnUTcOi3y29blnghK7fF3+GFV5xoSQ4ItXR/fI0/sfnGJUrLWYfT9M6J8Nzpn8YmR1fX7mrmcfJlfl80XzO7O+RUeg7Cwxb3ner18vvFkoYZKya4ekOy3JKKHWot/zPHnx4KAWFJC2wJ5wdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('2f118a3c37b90dd452b156c46a1e7539')
 
 
 @app.route("/callback", methods=['POST'])
@@ -36,9 +36,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='你吃飽了嗎'))
 
 
 if __name__ == "__main__":
